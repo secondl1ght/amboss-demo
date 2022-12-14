9f3596cd5002baaf83dd8cb047df544b89007169
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-14 11:58:07 -0700

    initial home page draft

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,6 +1,31 @@
-import Head from "next/head";
-import Image from "next/image";
-
 export default function Home() {
-  return <h1>Amboss Demo</h1>;
+  return (
+    <main className="w-full p-8">
+      <div className="text-white text-center space-y-2 w-full md:w-96 mx-auto">
+        <img src="/images/amboss.svg" alt="Amboss" className="w-full" />
+
+        <h1 className="text-3xl font-semibold">DEMO</h1>
+
+        <p className="bg-white/[0.03] rounded-lg p-8">
+          Trying out the{" "}
+          <a
+            href="https://amboss.space/"
+            className="text-link hover:text-hover"
+          >
+            AMBOSS.SPACE
+          </a>{" "}
+          API!
+
+          <br />
+          <br />
+
+          Use the following URL format to view node channel information:{" "}
+          <code className="break-all md:break-normal">
+            https://ambossdemo.space/
+            <span className="text-hover">[pubkey]</span>
+          </code>
+        </p>
+      </div>
+    </main>
+  );
 }
