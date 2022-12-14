d64e2ae1c70b360d8b02c88ba523ee5f8b5b24f4
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-14 11:53:17 -0700

    create copy button component

---
+++ components/CopyButton.tsx
@@ -0,0 +1,43 @@
+import { useState } from "react";
+
+export default function CopyButton({ content }) {
+  const [copied, setCopied] = useState(false);
+  const copy = () => {
+    navigator.clipboard.writeText(content);
+    setCopied(true);
+    setTimeout(() => setCopied(false), 2100);
+  };
+
+  return (
+    <button onClick={copy} className="bg-link/25 p-2 rounded-md text-white">
+      {copied ? (
+        <svg
+          className="w-6 h-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          data-v-4fa90e7f=""
+        >
+          <path
+            strokeLinecap="round"
+            strokeLinejoin="round"
+            d="M6 13.295L10.913 18 18 6"
+          ></path>
+        </svg>
+      ) : (
+        <svg
+          className="w-6 h-6"
+          xmlns="http://www.w3.org/2000/svg"
+          fill="none"
+          viewBox="0 0 24 24"
+          stroke="currentColor"
+          data-v-4fa90e7f=""
+        >
+          <rect width="9" height="13" x="6.5" y="6.5" rx="1.5"></rect>
+          <path d="M8.5 6A1.5 1.5 0 0110 4.5h6A1.5 1.5 0 0117.5 6v10a1.5 1.5 0 01-1.5 1.5"></path>
+        </svg>
+      )}
+    </button>
+  );
+}
