3e5d70260fb8432b6bfbdea1d45b6d9d50c4287e
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-23 16:34:00 -0700

    set font size in pixels

--- tailwind.config.js
+++ tailwind.config.js
@@ -5,6 +5,21 @@ module.exports = {
     "./components/**/*.{js,ts,jsx,tsx}",
   ],
   theme: {
+    fontSize: {
+      xs: ["12px", "16px"],
+      sm: ["14px", "20px"],
+      base: ["16px", "24px"],
+      lg: ["18px", "28px"],
+      xl: ["20px", "28px"],
+      "2xl": ["24px", "32px"],
+      "3xl": ["30px", "36px"],
+      "4xl": ["36px", "40px"],
+      "5xl": ["48px", "1"],
+      "6xl": ["60px", "1"],
+      "7xl": ["72px", "1"],
+      "8xl": ["96px", "1"],
+      "9xl": ["128px", "1"],
+    },
     extend: {
       colors: {
         link: "#A6B0C3FF",
