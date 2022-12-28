1ffe2d6f8706c0636bede037baa0c96e5b50cc5e
secondl1ght <85003930+secondl1ght@users.noreply.github.com>
2022-12-27 21:48:28 -0700

    remove not needed fields from query

--- queries/channels.ts
+++ queries/channels.ts
@@ -17,29 +17,9 @@ export const GET_CHANNELS = gql`
             list {
               block_age
               capacity
-              chan_point
               last_update
               last_update_date
               long_channel_id
-              node1_policy {
-                disabled
-                fee_base_msat
-                fee_rate_milli_msat
-                last_update
-                max_htlc_msat
-                min_htlc
-                time_lock_delta
-              }
-              node1_pub
-              node2_policy {
-                disabled
-                fee_base_msat
-                fee_rate_milli_msat
-                last_update
-                max_htlc_msat
-                min_htlc
-                time_lock_delta
-              }
               node2_pub
               short_channel_id
               node2_info {
